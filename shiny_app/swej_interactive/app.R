30cca129ac1eca1445f90f0f344f4d9a619057d1
Shawn Xu <shawnhxu@bu.edu>
2022-12-15 14:21:02 -0500

    shiny app

--- shiny_app/swej_interactive/app.R
+++ shiny_app/swej_interactive/app.R
@@ -7,45 +7,65 @@
 #    http://shiny.rstudio.com/
 #
 
+library(tidyverse)
 library(shiny)
 
-# Define UI for application that draws a histogram
+modelData <- read.csv("modelData.csv")
+colnames(modelData)[3] <- "Gender"
+colnames(modelData)[4] <- "Age"
+colnames(modelData)[5] <- "Ethnicity"
+age_plot <- ggplot(modelData, aes(x = Age, y = MAP)) + 
+  geom_smooth(method = "gam") 
+#initialize modelData dataset and change column names to be called later
+#also intialize age_plot to facet
+
+var_choices <- c("Gender", "Year", "Ethnicity")
+
 ui <- fluidPage(
+  selectInput(inputId = "var",
+              label = "Choose a Variable",
+              choices = var_choices),
+  plotOutput("facet")
+)
+server <- function(input, output) {
+  output$facet <- renderPlot({
+    age_plot + facet_wrap( ~ modelData[,input$var])+
+      labs(title = "Mean Arterial Pressure (MAP) vs. Age", 
+           x = "Age", y = "Mean Arterial Pressure")
+  })
+}
+shinyApp(ui, server)
+
+# Define UI for application that draws a histogram
+#ui <- fluidPage(
 
     # Application title
-    titlePanel("Pre and Post-Pandemic Health Trends Data"),
+    #titlePanel("General Health Trends in Relation to "),
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
+    # Show a plot of the generated distribution
+    #mainPanel(plotOutput("distPlot")),
 
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
-)
+    #selectInput(inputId = "var",
+                #label = "Health Variable",
+                #choices = var_choices),
+    
+    #plotOutput("plotMAP")
+#)
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+#server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
+    #output$distPlot <- renderPlot({
         # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+        #x    <- faithful[, 2]
+        #bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
         # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
-}
+        #hist(x, breaks = bins, col = 'darkgray', border = 'white',
+             #xlab = 'Waiting time to next eruption (in mins)',
+             #main = 'Histogram of waiting times')
+    #})
+#}
 
 # Run the application 
 shinyApp(ui = ui, server = server)
